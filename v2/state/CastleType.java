d19d63485a605c17f25f741d31109d3129137443
ostrovsky.alex <ostrovsky.alex@a51b5712-02d0-11de-9992-cbdf800730d7>
2009-02-25 00:19:13 +0000

    initial commit

---
+++ v2/state/CastleType.java
@@ -0,0 +1,16 @@
+package v2.state;
+
+/**
+ * User: alexo
+ * Date: Feb 21, 2009
+ * Time: 10:18:24 PM
+ */
+public enum CastleType
+{
+    //--------------------------------------------------------------------
+    QUEENSIDE, KINGSIDE;
+
+
+    //--------------------------------------------------------------------
+    public static final CastleType[] VALUES = values();
+}
