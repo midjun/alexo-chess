fdb2cdb49b3bb6bf29ccd901b4e63df3f40adf64
ostrovsky.alex <ostrovsky.alex@a51b5712-02d0-11de-9992-cbdf800730d7>
2009-02-25 00:18:57 +0000

    initial commit

---
+++ v2/state/MoveType.java
@@ -0,0 +1,19 @@
+package v2.state;
+
+/**
+ * Date: Feb 6, 2009
+ * Time: 6:28:35 PM
+ */
+public enum MoveType
+{
+    //--------------------------------------------------------------------
+    MOBILITY,
+    CAPTURE,
+    EN_PASSANT,
+    CASTLE,
+    PROMOTION;
+
+
+    //--------------------------------------------------------------------
+    public static final MoveType[] VALUES = values();
+}
