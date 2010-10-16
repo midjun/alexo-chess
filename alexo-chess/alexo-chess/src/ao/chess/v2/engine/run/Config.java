cd06f0f4ef954bbe82f01d6a5e926d1a19a594b1
ostrovsky.alex <ostrovsky.alex@a51b5712-02d0-11de-9992-cbdf800730d7>
2010-10-16 17:45:35 +0000

    * Cleaning up, extracting old-original branch

---
+++ alexo-chess/alexo-chess/src/ao/chess/v2/engine/run/Config.java
@@ -0,0 +1,36 @@
+package ao.chess.v2.engine.run;
+
+import ao.util.io.Dir;
+
+import java.io.File;
+
+/**
+ * User: aostrovsky
+ * Date: 18-Oct-2009
+ * Time: 11:07:37 PM
+ */
+public class Config
+{
+    //--------------------------------------------------------------------
+    private Config() {}
+
+
+    //--------------------------------------------------------------------
+    private static String workingDirectory = "";
+
+
+    //--------------------------------------------------------------------
+    public static String workingDirectory() {
+        return workingDirectory;
+    }
+
+    public static void setWorkingDirectory(String workingDir) {
+        workingDirectory = workingDir;
+    }
+
+
+    //--------------------------------------------------------------------
+    public static File dir(String relativePath) {
+        return Dir.get(workingDirectory + relativePath);
+    }
+}
