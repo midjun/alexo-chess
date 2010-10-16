49bf804166d39ae77d102f93a77a811a28b61d98
ostrovsky.alex <ostrovsky.alex@a51b5712-02d0-11de-9992-cbdf800730d7>
2010-10-16 17:41:20 +0000

    * Cleaning up, extracting old-original branch

---
+++ src/chess/v2/state/tablebase/MaterialOracle.java
@@ -0,0 +1,183 @@
+package ao.chess.v2.state.tablebase;
+
+import ao.chess.v2.piece.Piece;
+import ao.chess.v2.state.Move;
+import ao.chess.v2.state.Outcome;
+import ao.chess.v2.state.State;
+import ao.util.time.Stopwatch;
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
+import it.unimi.dsi.fastutil.longs.LongSet;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * User: aostrovsky
+ * Date: 13-Oct-2009
+ * Time: 11:15:38 PM
+ */
+public class MaterialOracle implements Serializable
+{
+    //--------------------------------------------------------------------
+    private final LongSet blackWins = new LongOpenHashSet();
+    private final LongSet whiteWins = new LongOpenHashSet();
+
+
+    //--------------------------------------------------------------------
+    public MaterialOracle(
+            final Oracle      oracle,
+            final List<Piece> material)
+    {
+        System.out.println("Computing MaterialOracle for " + material);
+        Stopwatch timer = new Stopwatch();
+
+        StateMap states = new StateMap();
+        new PositionTraverser().traverse(
+                material, states);
+
+        System.out.println("filled state map, took " + timer);
+        timer = new Stopwatch();
+
+        HashRetrograde retro = new HashRetrograde();
+        states.traverse(retro);
+
+        System.out.println("retrograte analysis done, took " + timer);
+        timer = new Stopwatch();
+
+        // all initial mates
+        LongSet prevWhiteWins = new LongOpenHashSet();
+        LongSet prevBlackWins = new LongOpenHashSet();
+
+        addImmediateMates(
+                states.states(), oracle, states,
+                prevWhiteWins, prevBlackWins);
+
+        int ply = 1;
+        System.out.println("initial mates found, took " + timer);
+        timer = new Stopwatch();
+
+        while (! prevWhiteWins.isEmpty() ||
+               ! prevBlackWins.isEmpty())
+        {
+            whiteWins.addAll( prevWhiteWins );
+            blackWins.addAll( prevBlackWins );
+
+            LongSet nextWhiteWins = new LongOpenHashSet();
+            LongSet nextBlackWins = new LongOpenHashSet();
+
+            addNextImmediates(states, retro, prevWhiteWins, oracle,
+                              nextWhiteWins, nextBlackWins);
+            addNextImmediates(states, retro, prevBlackWins, oracle,
+                              nextWhiteWins, nextBlackWins);
+
+            System.out.println(
+                    "finished ply " + (ply++) + ", took " + timer);
+            timer = new Stopwatch();
+
+            prevWhiteWins = nextWhiteWins;
+            prevBlackWins = nextBlackWins;
+        }
+
+        System.out.println("done, got " +
+                blackWins.size() + " | " +
+                whiteWins.size() + " | " +
+                states.size());
+    }
+
+
+    //--------------------------------------------------------------------
+    private void addNextImmediates(
+            StateMap       allStates,
+            HashRetrograde retro,
+            LongSet        prevWins,
+            Oracle         oracle,
+            LongSet        nextWhiteWins,
+            LongSet        nextBlackWins)
+    {
+        for (long wWin : prevWins) {
+            addImmediateMates(
+                    allStates.states(retro.precedents( wWin )),
+                    oracle, allStates, nextWhiteWins, nextBlackWins);
+        }
+    }
+
+
+    //--------------------------------------------------------------------
+    private void addImmediateMates(
+            Iterable<State> states,
+            Oracle          oracle,
+            StateMap        allStates,
+            LongSet         nextWhiteWins,
+            LongSet         nextBlackWins)
+    {
+        for (State state : states) {
+            if (! isUnknown(state)) continue;
+
+            Outcome result = findImminentMate(
+                    state, allStates, oracle);
+            if (result == Outcome.WHITE_WINS) {
+                nextWhiteWins.add( state.staticHashCode() );
+            } else if (result == Outcome.BLACK_WINS) {
+                nextBlackWins.add( state.staticHashCode() );
+            }
+        }
+    }
+
+    private boolean isUnknown(State state) {
+        long staticHash = state.staticHashCode();
+        return ! (whiteWins.contains(staticHash) ||
+                  blackWins.contains(staticHash));
+    }
+
+
+    //--------------------------------------------------------------------
+    private Outcome findImminentMate(
+            State state, StateMap allStates, Oracle oracle)
+    {
+        Outcome result = state.knownOutcome();
+        if (result != null && result != Outcome.DRAW) {
+            return result;
+        }
+
+        int legalMoves[] = state.legalMoves();
+        if (legalMoves == null) return null;
+
+        int ties = 0, losses = 0;
+        for (int legalMove : legalMoves)
+        {
+            Move.apply(legalMove, state);
+
+            long    staticHash     = state.staticHashCode();
+            Outcome imminentResult =
+                    allStates.containsState( staticHash )
+                    ? see( staticHash )
+                    : oracle.see(state);
+            Move.unApply(legalMove, state);
+
+            if (imminentResult != null &&
+                    imminentResult != Outcome.DRAW) {
+                if (state.nextToAct() == imminentResult.winner()) {
+                    return imminentResult;
+                } else {
+                    losses++;
+                }
+            } else {
+                ties++;
+            }
+        }
+
+        return   ties   > 0 ? null
+               : losses > 0 ? Outcome.loses( state.nextToAct() )
+                            : null;
+    }
+
+
+    //--------------------------------------------------------------------
+    public Outcome see(long staticHash) {
+        return whiteWins.contains(staticHash)
+               ? Outcome.WHITE_WINS
+               : blackWins.contains(staticHash)
+                 ? Outcome.BLACK_WINS
+                 : null;
+    }
+}
---
+++ src/chess/v2/state/tablebase/PositionTraverser.java
@@ -0,0 +1,218 @@
+package ao.chess.v2.state.tablebase;
+
+import ao.chess.v2.data.Location;
+import ao.chess.v2.piece.Colour;
+import ao.chess.v2.piece.Figure;
+import ao.chess.v2.piece.Piece;
+import ao.chess.v2.state.Outcome;
+import ao.chess.v2.state.State;
+import ao.util.misc.Traverser;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * User: aostrovsky
+ * Date: 13-Oct-2009
+ * Time: 11:17:46 PM
+ */
+public class PositionTraverser
+{
+    //--------------------------------------------------------------------
+    public static void main(String[] args) {
+        long before = System.currentTimeMillis();
+
+        final long count[] = {0};
+
+        new PositionTraverser().traverse(
+                Arrays.asList(
+                        Piece.WHITE_KING,
+                        Piece.BLACK_KING),
+                new Traverser<State>() {
+                    @Override public void traverse(State state) {
+                        System.out.println();
+                        System.out.println(state);
+
+                        count[0]++;
+                    }
+                }
+        );
+
+        System.out.println("found: " + count[0]);
+        System.out.println(
+                "took " + (System.currentTimeMillis() - before));
+    }
+
+
+    //--------------------------------------------------------------------
+    private final Piece[][] BOARD;
+
+
+    //--------------------------------------------------------------------
+    public PositionTraverser()
+    {
+        BOARD = new Piece[ Location.RANKS ]
+                         [ Location.FILES ];
+    }
+
+
+    //--------------------------------------------------------------------
+    public synchronized void traverse(
+            List     <Piece> pieces,
+            Traverser<State> visitor)
+    {
+        place(pieces.get(0),
+              pieces.subList(1, pieces.size()),
+              visitor);
+    }
+
+
+    //--------------------------------------------------------------------
+    private void place(
+            Piece            first,
+            List     <Piece> rest,
+            Traverser<State> visitor)
+    {
+        for (int rank = 0; rank < Location.RANKS; rank++) {
+            if (first.figure() == Figure.PAWN &&
+                    (rank == 0 || rank == 7)) continue;
+
+            for (int file = 0; file < Location.FILES; file++) {
+                if (BOARD[rank][file] != null) continue;
+
+                BOARD[rank][file] = first;
+
+                if (rest.isEmpty()) {
+                    check(Colour.WHITE, visitor);
+                    check(Colour.BLACK, visitor);
+                } else {
+                    place(rest.get(0),
+                          rest.subList(1, rest.size()),
+                          visitor);
+                }
+
+                BOARD[rank][file] = null;
+            }
+        }
+    }
+
+
+    //--------------------------------------------------------------------
+    private void check(
+            Colour           nextToAct,
+            Traverser<State> visitor)
+    {
+        checkValid(new State(
+                fen(nextToAct, -1)), visitor);
+
+        int enPassant = enPassant( nextToAct.invert() );
+        if (enPassant == -1) return;
+
+        checkValid(new State(
+                fen(nextToAct, enPassant)), visitor);
+    }
+
+    private void checkValid(
+            State            state,
+            Traverser<State> visitor)
+    {
+        if (! state.checkPieces()) {
+            return;
+        }
+
+        boolean lastToActInCheck =
+                state.isInCheck( state.nextToAct().invert() );
+
+        // cannot move into check
+        if (lastToActInCheck) return;
+
+        visitor.traverse(state);
+    }
+
+
+    //--------------------------------------------------------------------
+    private int enPassant(Colour by) {
+        if (by == Colour.WHITE) {
+            for (int file = 0; file < Location.FILES; file++) {
+                if (BOARD[3][file] == Piece.WHITE_PAWN &&
+                        BOARD[2][file] == null &&
+                        BOARD[1][file] == null) {
+                    return file;
+                }
+            }
+        } else {
+            for (int file = 0; file < Location.FILES; file++) {
+                if (BOARD[4][file] == Piece.BLACK_PAWN &&
+                        BOARD[5][file] == null &&
+                        BOARD[6][file] == null) {
+                    return file;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    //--------------------------------------------------------------------
+    private String fen(Colour nextToAct, int enPassant)
+    {
+        StringBuilder str = new StringBuilder();
+
+        for (int rank = 7; rank >= 0; rank--) {
+            int emptySquares = 0;
+            for (int file = 0; file < 8; file++) {
+                Piece p = BOARD[rank][file];
+                if (p == null) {
+                    emptySquares++;
+                } else {
+                    if (emptySquares > 0) {
+                        str.append(emptySquares);
+                        emptySquares = 0;
+                    }
+                    str.append(p.toString());
+                }
+            }
+            if (emptySquares > 0) {
+                str.append(emptySquares);
+            }
+
+            if (rank != 0 ) {
+                str.append("/");
+            }
+        }
+
+        str.append(" ");
+        str.append(nextToAct == Colour.WHITE
+                   ? "w" : "b");
+        str.append(" ");
+
+        // castles
+        str.append("-");
+
+        str.append(" ");
+
+		// En passant square
+        if (enPassant == -1) {
+            str.append("-");
+        } else {
+            str.append(State.FILES.charAt(enPassant));
+//            str.append(" ");
+            if (nextToAct == Colour.WHITE) {
+                str.append(6);
+            } else {
+                str.append(3);
+            }
+        }
+        str.append(" ");
+
+        // reversible moves
+        str.append("0");
+
+        str.append(" ");
+
+        // full moves since start of game
+        str.append("0");
+
+		return str.toString();
+    }
+}
